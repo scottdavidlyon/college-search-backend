d52ae1af433f66aa0525cf48e4b2fbf52956370e
zmartin02740 <martin.zachary.e@gmail.com>
2020-08-07 08:53:50 -0400

    finalized backend

--- app.py
+++ app.py
@@ -1,47 +1,12 @@
 
 from flask import Flask, request
-from flask_cors import CORS, cross_origin
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask_cors import CORS
 from sqlalchemy import create_engine
-from sqlalchemy.dialects.postgresql import JSON
 import pandas as pd
 import json
-import csv
-
-
-with open('ma_schools.json') as f:
-    data_ms = json.load(f)
-
-with open('programs.json') as f:
-    data_p = json.load(f)
 
 app = Flask(__name__)
 CORS(app)
-# app.config['CORS_HEADERS'] = 'Content-Type'
-
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://telbfierjjqges:5fbcf8bb02daf595e0d47d9b366265cf64af7543ddbcd061f365bb138bb179dc@ec2-52-1-95-247.compute-1.amazonaws.com:5432/datbtt39dmvo1o'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-
-# programs = []
-# for program in data_ms:
-#     programs.append(program)
-
-# db = SQLAlchemy(app)
-
-
-# class MASchools(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     school = db.Column(JSON)
-
-
-# for college in programs:
-#     db.session.add(MASchools(school=college))
-#     db.session.commit()
-
-
-# alchemyEngine = create_engine(
-#     'postgresql+psycopg2://zack:zem1992@127.0.0.1/college_search', pool_recycle=3600)
 
 alchemyEngine = create_engine(
     'postgres://telbfierjjqges:5fbcf8bb02daf595e0d47d9b366265cf64af7543ddbcd061f365bb138bb179dc@ec2-52-1-95-247.compute-1.amazonaws.com:5432/datbtt39dmvo1o', pool_recycle=3600)
@@ -62,9 +27,6 @@ for index, row in df_programs.iterrows():
 fields = []
 for index, row in df_fields.iterrows():
     fields.append(row['field'])
-
-data_ms = json.dumps(data_ms)
-data_p = json.dumps(data_p)
 
 
 @app.route("/ma-schools", methods=['GET'])
