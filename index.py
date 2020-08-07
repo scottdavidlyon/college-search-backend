0921846b0c93f89a50e7bec5cc6358095cc77ebd
zmartin02740 <martin.zachary.e@gmail.com>
2020-08-06 20:45:18 -0400

    changes

--- index.py
+++ index.py
@@ -9,40 +9,46 @@ import pandas as pd
 import json
 import csv
 
+
+with open('ma_schools.json') as f:
+    data_ms = json.load(f)
+
+with open('programs.json') as f:
+    data_p = json.load(f)
+
 app = Flask(__name__)
 CORS(app)
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://zack:zem1992@127.0.0.1/college_search'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://telbfierjjqges:5fbcf8bb02daf595e0d47d9b366265cf64af7543ddbcd061f365bb138bb179dc@ec2-52-1-95-247.compute-1.amazonaws.com:5432/datbtt39dmvo1o'
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 # programs = []
-# for program in data_p:
-#     p = {}
-#     p[program] = data_p[program]
-#     programs.append(p)
+# for program in data_ms:
+#     programs.append(program)
 
 # print(programs)
 
 # db = SQLAlchemy(app)
 
 
-# class Programs(db.Model):
+# class MASchools(db.Model):
 #     id = db.Column(db.Integer, primary_key=True)
-#     program = db.Column(JSON)
+#     school = db.Column(JSON)
 
 
 # for college in programs:
-#     db.session.add(Programs(program=college))
+#     db.session.add(MASchools(school=college))
 #     db.session.commit()
 
 
 # alchemyEngine = create_engine(
 #     'postgresql+psycopg2://zack:zem1992@127.0.0.1/college_search', pool_recycle=3600)
+
 alchemyEngine = create_engine(
-    'postgres://pmykabxshnofzn:6ad7d89187806b5a1d4e11303d66a3949639ebdc325121a485a1c2dad9570dc9@ec2-54-197-254-117.compute-1.amazonaws.com:5432/da3bms0hdo4bp7', pool_recycle=3600)
+    'postgres://telbfierjjqges:5fbcf8bb02daf595e0d47d9b366265cf64af7543ddbcd061f365bb138bb179dc@ec2-52-1-95-247.compute-1.amazonaws.com:5432/datbtt39dmvo1o', pool_recycle=3600)
 dbConnection = alchemyEngine.connect()
 df_programs = pd.read_sql("select * from \"programs\"", dbConnection)
-df_schools = pd.read_sql("select * from \"ma_school\"", dbConnection)
+df_schools = pd.read_sql("select * from \"ma_schools\"", dbConnection)
 df_fields = pd.read_sql("select * from \"fields\"", dbConnection)
 pd.set_option('display.expand_frame_repr', False)
 
@@ -57,6 +63,9 @@ for index, row in df_programs.iterrows():
 fields = []
 for index, row in df_fields.iterrows():
     fields.append(row['field'])
+
+data_ms = json.dumps(data_ms)
+data_p = json.dumps(data_p)
 
 
 @app.route("/ma-schools")
